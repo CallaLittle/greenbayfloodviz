7feddca414fe4814f3d481fe72459b2d6648b927
CallaLittle <clittle723@gmail.com>
2020-01-28 07:43:37 -0600

    Imagery from https

--- js/custom.js
+++ js/custom.js
@@ -87,7 +87,7 @@
 		}).addTo(map);
 
 	//satellite tileset
-	var HERE_hybridDay = L.tileLayer('http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/{type}/{mapID}/hybrid.day/{z}/{x}/{y}/{size}/{format}?app_id={app_id}&app_code={app_code}&lg={language}', {
+	var HERE_hybridDay = L.tileLayer('https://{s}.{base}.maps.cit.api.here.com/maptile/2.1/{type}/{mapID}/hybrid.day/{z}/{x}/{y}/{size}/{format}?app_id={app_id}&app_code={app_code}&lg={language}', {
 		attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
 		subdomains: '1234',
 		mapID: 'newest',
